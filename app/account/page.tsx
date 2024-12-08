b96c4479d661e9f57a8b3a7932102afb89508f75
Manish Chaudhari <chaudharimanish07@gmail.com>
2024-12-08 11:25:20 -0500

    created register and account routes

---
+++ app/account/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div>All the accounts created so far</div>;
+};
+
+export default Page;
---
+++ app/register/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div>Register for an account here</div>;
+};
+
+export default Page;
